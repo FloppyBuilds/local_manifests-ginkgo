0487fde18c4802c270846ac16d86bdc9897454f2
Flopster101 <nahuelgomez329@gmail.com>
2025-12-21 17:35:36 +0000

    reclone-trees: Actually use BRANCH variable
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- reclone-trees.sh
+++ reclone-trees.sh
@@ -17,7 +17,7 @@ fi
 for m in "${DEVICES[@]}"; do
   sed -i "/$m/d" "$XML"
   [ -d "$(pwd)/device/xiaomi/$m" ] && rm -rf "$(pwd)/device/xiaomi/$m"
-  git clone -j"$(nproc --all)" "$URL/android_device_xiaomi_$m" "$(pwd)/device/xiaomi/$m"
+  git clone -j"$(nproc --all)" -b "$BRANCH" --single-branch "$URL/android_device_xiaomi_$m" "$(pwd)/device/xiaomi/$m"
   [ -d "$(pwd)/vendor/xiaomi/$m" ] && rm -rf "$(pwd)/vendor/xiaomi/$m"
-  git clone -j"$(nproc --all)" "$URL/proprietary_vendor_xiaomi_$m" "$(pwd)/vendor/xiaomi/$m"
+  git clone -j"$(nproc --all)" -b "$BRANCH" --single-branch "$URL/proprietary_vendor_xiaomi_$m" "$(pwd)/vendor/xiaomi/$m"
 done
